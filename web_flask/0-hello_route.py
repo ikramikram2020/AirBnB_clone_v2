40a4cdc6e6d461c5fe1f1e8806a7ead4b0a421d7
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-08-20 15:35:35 +0100

    Starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 from flask import Flask
 
-# Create an instance of the Flask class
 app = Flask(__name__)
 
 
-# Define a route for the root URL ("/") with strict_slashes set to False
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def home():
     return "Hello HBNB!"
 
 
-# Check if the executed file is the main program and run the app
 if __name__ == '__main__':
-    # Start the Flask application listening on 0.0.0.0, port 5000
     app.run(host='0.0.0.0', port=5000)
